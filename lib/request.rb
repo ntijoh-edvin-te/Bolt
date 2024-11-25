0be6c165de6f0103abebc12f4427a1d9bea68af7
Edvin Tengvall <edvin.tengvall@elev.ga.ntig.se>
2024-11-25 22:53:09 +0100

    Upd

--- lib/request.rb
+++ lib/request.rb
@@ -9,29 +9,46 @@ class Server
     def start
         while (session = @server.accept)
             puts "Client connected..."
-
             payload = [] 
             session.read.each_line {|line| payload << line}
-            
             payload.each {|i| puts "#{i}"}
         end
     end
 end
 
+# (1) Server (class) establishes a request/session
+# (2) Request (class) handles request - (A) is_allowed? (B) Method? (C) GET--> Resources? + Parameters? || POST--> Request Message Body?
+#
+# (3) 
+
 class Request
-    def initialize(http_payload)
-      @hash = {}
-      parser(http_payload)
+    def initialize(request)
+        @request_data = {}
+        @allowed_methods = ["GET", "POST"]
     end
-  
-    def parser(http_payload)
-        http_payload.each_line.with_index  do |line,i|
+
+
+    # (A) Request Line      [ex] GET /fruits?type=apple HTTP/1.1
+    # (B) Request Headers       [ex] Host: fruits.com
+    # (C) Request Message Body      [ex] username=apple&password=kiwi
+
+
+    def handler
+        
+    end
+
+    def is_allowed?(request)
+        return @allowed_methods.include?(File.read(request).lines.first.split(" ")[0])
+    end
+
+    def parser(request)
+        File.read(request).each_line.with_index do |line,i|
             if i==0
-                @hash["Method"] = line.strip
+                @request_data["Method"], @request_data["Resource"], @request_data["Version"] = line.split(" ")
             else
                 key, value = line.split(":", 2)
                 if key && value
-                    @hash[key.strip] = value.strip
+                    @request_data[key.strip] = value.strip
                 end
             end
         end
@@ -39,15 +56,32 @@ class Request
 
     def method_missing(m, *args, &block)
         header = m.to_s.split("_").join("-").downcase
-
-        matching_key = @hash.keys.find {|k| k.downcase==header}
-        
+        matching_key = @request_data.keys.find {|k| k.downcase==header}
         if matching_key 
-            return @hash[matching_key] 
+            return @request_data[matching_key] 
         else
-            super
+            puts "Method #{matching_key} does not exist. "
         end
     end
 end
 
-puts Request.new(IO.read("./http_payload.txt"))
+def get_resource(resource)
+    data = []
+    keys = []
+
+    IO.read(resource).lines.each_with_index do |line, i|
+        if i == 0
+            keys = line.strip.split(",").map(&:strip)
+        else
+            values = line.strip.split(",").map(&:strip)
+            data << keys.zip(values).to_h
+        end
+    end
+    data
+end
+
+
+
+# get_resource('resources\heroes.txt')
+
+Request.new("http_payload.txt")
