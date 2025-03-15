a3b74823a3c470302135bd6840d253bdd5872365
ntijoh-edvin-te <edvin.tengvall@elev.ga.ntig.se>
2025-03-15 21:14:30 +0100

    Reworks

--- lib/WebServer.rb
+++ lib/WebServer.rb
@@ -2,32 +2,31 @@
 
 require 'socket'
 require_relative 'Request'
-require_relative 'Router'
 require_relative 'AuthContext'
 
-class Server
-    def initialize(log_device)
-        start_server(8000)
+class WebServer
+    attr_reader :log_device, :router
+
+    def initialize(log_device, router)
+        @log_device = log_device
+        @router = router
     end
 
-    private
-
-    def log
-
-    def start_server(port)
+    def start_server(port = 8080)
+        log_device.log("Server is starting: http://127.0.0.1:#{port}")
         socket = TCPServer.new('localhost', port)
         session_handler(socket)
     end
 
+    private
+
     def session_handler(socket)
         while (session = socket.accept)
-            @logger.info("New connection: #{session.peeraddr(:numeric)[2]}.")
             begin
                 request = Request.new(@logger, session)
                 route = @router.route(request)
                 auth_context = AuthContext.new(@logger, request)
             rescue Exception => e
-                @logger.error("Request processing error: #{e}")
             ensure
                 session.close
             end
--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'lib/LogDevice'
+require_relative 'lib/WebServer'
+require_relative 'lib/Router'
 
 log_device = LogDevice.new
+router = Router.new(log_device)
+web_server = WebServer.new(log_device, router)
+
+web_server.start_server
